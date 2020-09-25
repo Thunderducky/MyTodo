a5f73f895918c46eea648a9902c2f311a89d093a
Thunderducky <scottzillaster@gmail.com>
2020-09-25 12:02:13 -0500

    added the ability to add elements to the page, fixes #4

--- logic.js
+++ logic.js
@@ -1,9 +1,33 @@
 var newTodoEl = document.getElementById("new-todo");
 var newTodoForm = document.getElementById("new-todo-form");
+var todosDiv = document.getElementById("todos");
 
+var todos = ["Eat Lunch", "Drink Water", "Stretch", "Walk Dog"];
+
+function renderTodos(){
+    todosDiv.innerHTML = "";
+    for(var i = 0; i < todos.length; i++){
+        var todo = todos[i];
+       var todoDiv = document.createElement("div");
+       todoDiv.setAttribute("class", "todo")
+       var todoH2 = document.createElement("h2");
+       todoH2.textContent = todo;
+       var todoButton = document.createElement("button");
+       todoButton.textContent = "Done";
+
+        todoDiv.appendChild(todoH2);
+        todoDiv.appendChild(todoButton);
+        todosDiv.appendChild(todoDiv);
+        
+    }
+}
 
 newTodoForm.addEventListener("submit", function(event){
     event.preventDefault();
     var value = newTodoEl.value;
-    console.log(value);
+    newTodoEl.value = "";
+    todos.push(value);
+    renderTodos();
 })
+
+renderTodos();
